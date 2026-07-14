d814d16a5ee3114c516173a78e2bcd8be8987deb
synax739 <yahyanarin79@gmail.com>
2026-07-14 11:25:27 +0300

    Update drawing logic in Arguman.lua

--- Arguman.lua
+++ Arguman.lua
@@ -1,1 +1,10 @@
-print(Drawing and "Drawing var" or "Drawing YOK")
+local test = Drawing.new("Square")
+if test then
+    test.Visible = true
+    test.Size = Vector2.new(100, 100)
+    test.Position = Vector2.new(100, 100)
+    test.Color = Color3.new(1,0,0)
+    print("Drawing ÇALIŞIYOR")
+else
+    print("Drawing ÇALIŞMIYOR - Delta'yı güncelle ya da farklı bir exploit dene")
+end
